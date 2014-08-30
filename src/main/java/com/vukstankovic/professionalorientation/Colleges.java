2af22e34257f5aeb60469607e5978af0dd104ae7
Vuk Stanković <vuks89@gmail.com>
2014-08-30 14:05:54 +0200

    Added college recommendations

--- src/main/java/com/vukstankovic/professionalorientation/Colleges.java
+++ src/main/java/com/vukstankovic/professionalorientation/Colleges.java
@@ -4,6 +4,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,13 +21,15 @@ import com.google.gson.Gson;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 import com.vukstankovic.professionalorientation.classes.College;
-import com.vukstankovic.professionalorientation.classes.SchoolType;
+import com.vukstankovic.professionalorientation.helpers.ValueComparator;
 import com.vukstankovic.professionalorientation.config.DBConnect;
 
 @Path("colleges")
 public class Colleges {
 	public String response = "Nothing to respond";
 	public ArrayList<College> colleges = new ArrayList<College>();
+	HashMap<Integer,Double> interests = new HashMap<Integer,Double>();
+	ValueComparator bvc =  new ValueComparator(interests);
 	
 	@GET
     @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
@@ -40,23 +46,48 @@ public class Colleges {
             st = (Statement) con.createStatement();
             rs = st.executeQuery("SELECT * FROM users WHERE id = "+id);
             rs.next();
-
+            
             double poljoprivreda = rs.getDouble(10);
+            interests.put(1, poljoprivreda);
             double sumarstvo = rs.getDouble(11);
+            interests.put(2, sumarstvo);
             double geodezija = rs.getDouble(12);
+            interests.put(3, geodezija);
             double masinstvo = rs.getDouble(13);
+            interests.put(4, masinstvo);
             double elektrotehnika = rs.getDouble(14);
+            interests.put(5, elektrotehnika);
             double hemija = rs.getDouble(15);
+            interests.put(6, hemija);
             double tekstil = rs.getDouble(16);
+            interests.put(7, tekstil);
             double saobracaj = rs.getDouble(17);
+            interests.put(8, saobracaj);
             double trgovinaugostiteljstvo = rs.getDouble(18);
+            interests.put(9, trgovinaugostiteljstvo);
             double ekonomijapravo = rs.getDouble(19);
+            interests.put(10, ekonomijapravo);
             double hidrometeorologija = rs.getDouble(20);
+            interests.put(11, hidrometeorologija);
             double kultura = rs.getDouble(21);
+            interests.put(12, kultura);
             double zdravstvo = rs.getDouble(22);
+            interests.put(13, zdravstvo);
             double usluge = rs.getDouble(23);
+            interests.put(14, usluge);
+            
+            SortedMap<String,Double> sorted_interests = new TreeMap<String,Double>(bvc);
+            int first = Integer.parseInt(sorted_interests.lastKey());
+            sorted_interests.remove(first);
+            int second = Integer.parseInt(sorted_interests.lastKey());
+            sorted_interests.remove(second);
+            int third = Integer.parseInt(sorted_interests.lastKey());
+            sorted_interests.remove(third);
+            
             rs.close();
-            rs = st.executeQuery("SELECT * FROM colleges");
+            rs = st.executeQuery("SELECT * FROM colleges WHERE interest LIKE %'"+first+",'%"
+            		+ " AND interest LIKE %'"+second+",'%"
+            				+ " AND interest LIKE %'"+third+",'%");
             while (rs.next()) {
             	College c = new College();
             	c.setId(rs.getInt(1));
@@ -69,6 +100,45 @@ public class Colleges {
             	
             	colleges.add(c);
             }
+            
+            rs.close();
+            rs = st.executeQuery("SELECT * FROM colleges WHERE (interest LIKE %'"+first+",'%"
+            		+ " AND interest LIKE %'"+second+",'%) "
+            				+ "OR (interest LIKE %'"+first+",'% AND interest LIKE %'"+third+",'%)"
+            						+ "OR (interest LIKE %'"+second+",'% AND interest LIKE %'"+third+",'%)");
+            while (rs.next()) {
+            	College c = new College();
+            	c.setId(rs.getInt(1));
+            	c.setTitle(rs.getString(2));
+            	c.setProgramme(rs.getString(3));
+            	c.setAbbrevation(rs.getString(4));
+            	c.setArea(rs.getString(5));
+            	c.setDescription(rs.getString(6));
+            	c.setInterest(rs.getString(7));
+            	if(colleges.contains(c)){
+            		continue;
+            	}
+            	colleges.add(c);
+            }
+
+            rs.close();
+            rs = st.executeQuery("SELECT * FROM colleges WHERE (interest LIKE %'"+first+",'%"
+            				+ "OR (interest LIKE %'"+third+",'%)"
+            						+ "OR (interest LIKE %'"+second+",'%");
+            while (rs.next()) {
+            	College c = new College();
+            	c.setId(rs.getInt(1));
+            	c.setTitle(rs.getString(2));
+            	c.setProgramme(rs.getString(3));
+            	c.setAbbrevation(rs.getString(4));
+            	c.setArea(rs.getString(5));
+            	c.setDescription(rs.getString(6));
+            	c.setInterest(rs.getString(7));
+            	if(colleges.contains(c)){
+            		continue;
+            	}
+            	colleges.add(c);
+            }
             String json = new Gson().toJson(colleges);
             return json;
 
@@ -78,7 +148,6 @@ public class Colleges {
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
 
         } catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
         	response = e.getMessage();
 			e.printStackTrace();
 		} finally {

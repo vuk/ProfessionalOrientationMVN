81d2349c46756ce9d042c1a84e5b4fe4dcb5f462
Vuk Stanković <vuks89@gmail.com>
2014-09-05 14:31:10 +0200

    Recommendations fix

--- src/main/java/com/vukstankovic/professionalorientation/helpers/CollegeComparator.java
+++ src/main/java/com/vukstankovic/professionalorientation/helpers/CollegeComparator.java
@@ -7,9 +7,26 @@ import com.vukstankovic.professionalorientation.classes.College;
 public class CollegeComparator implements Comparator<College> {
 	@Override
 	public int compare(College o1, College o2) {
-		Integer ponder1 = o1.getPonder();
-		Integer ponder2 = o2.getPonder();
+		Integer ponder1 = calculatePonder(o1);
+		Integer ponder2 = calculatePonder(o2);
 		return ponder1.compareTo(ponder2);
 	}
-
+	
+	
+	public int calculatePonder(College o1){
+		return (int) ((int) o1.getPonder()*getMultiplier(o1.getPriority()));
+	}
+	
+	public double getMultiplier(int i){
+		switch (i) {
+		case 1:
+			return 0.5;
+		case 2: 
+			return 0.45;
+		case 3: 
+			return 0.40;
+		default: 
+			return 0.0;
+		}
+	}
 }

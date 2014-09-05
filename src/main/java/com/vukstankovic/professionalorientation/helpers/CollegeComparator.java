6784f2aefc166dfa0919c079dd36aecd3ecd5cf2
Vuk Stanković <vuks89@gmail.com>
2014-09-05 13:51:45 +0200

    Removed limitations

--- src/main/java/com/vukstankovic/professionalorientation/helpers/CollegeComparator.java
+++ src/main/java/com/vukstankovic/professionalorientation/helpers/CollegeComparator.java
@@ -7,18 +7,9 @@ import com.vukstankovic.professionalorientation.classes.College;
 public class CollegeComparator implements Comparator<College> {
 	@Override
 	public int compare(College o1, College o2) {
-		Integer ponder1 = (int) Math.round(o1.getPonder()*this.getMultiplier(o1.getPriority()));
-		Integer ponder2 = (int) Math.round(o2.getPonder()*this.getMultiplier(o2.getPriority()));
+		Integer ponder1 = o1.getPonder();
+		Integer ponder2 = o2.getPonder();
 		return ponder1.compareTo(ponder2);
-	}
-	
-	private double getMultiplier(int i){
-		switch(i){
-		case 1 : return 0.5;
-		case 2 : return 0.48;
-		case 3 : return 0.46;
-		default : return 0.0;
-		}
 	}
 
 }

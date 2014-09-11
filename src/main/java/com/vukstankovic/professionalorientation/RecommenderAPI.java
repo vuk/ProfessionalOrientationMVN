093f2325a982f4cbaab9dcffc69f8aaa42d9d55e
Vuk Stanković <vuks89@gmail.com>
2014-09-12 01:27:27 +0200

    Added choices to users

--- src/main/java/com/vukstankovic/professionalorientation/RecommenderAPI.java
+++ src/main/java/com/vukstankovic/professionalorientation/RecommenderAPI.java
@@ -1,7 +1,5 @@
 package com.vukstankovic.professionalorientation;
 
-import java.util.ArrayList;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,8 +7,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
-import com.vukstankovic.professionalorientation.classes.User;
-import com.vukstankovic.recommender.Recommendations;
 import com.vukstankovic.recommender.Recommender;
 
 @Path("recommendation")
@@ -21,10 +17,7 @@ public class RecommenderAPI {
 	public String getSimilar(@QueryParam("id") String id){
 		Recommender rc = new Recommender();
 		rc.calculateSimilarity(Integer.parseInt(id));
-		Recommendations rcm = new Recommendations();
-		ArrayList<User> similar = rc.getSimilarUsers();
-		rcm.generateChoicesList(similar);
-		String json = new Gson().toJson(similar);
+		String json = new Gson().toJson(rc.getSimilarUsers());
 		return json;
 	}
 }

4e45e33deeb22aa39ac56c41ed4a531df8230717
Vuk Stanković <vuks89@gmail.com>
2014-09-13 11:37:21 +0200

    Bug fixing

--- src/main/java/com/vukstankovic/professionalorientation/classes/User.java
+++ src/main/java/com/vukstankovic/professionalorientation/classes/User.java
@@ -197,11 +197,11 @@ public class User {
         Statement st = null;
         ResultSet rs = null;
         setChoices(new ArrayList<Choice>());;
-        /*try {
+        try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = (Connection) DriverManager.getConnection(db.getUrl(), db.getUser(), db.getPassword());
 	        st = (Statement) con.createStatement();
-	        rs = st.executeQuery("SELECT * FROM choices WHERE user_id = "+getId());
+	        /*rs = st.executeQuery("SELECT * FROM choices WHERE user_id = "+getId());
 	        while(rs.next()){
 	        	Choice c = new Choice();
 	        	c.setId(rs.getInt(1));
@@ -209,14 +209,14 @@ public class User {
 	        	c.setCollege_id(rs.getInt(3));
 	        	c.setMark(rs.getInt(4));
 	        	choices.add(c);
-	        }
+	        }*/
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
         
 	}
 	

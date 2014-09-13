cdd42f684f5e2dc53cbeb17b1604b7d7d70f7ee6
Vuk Stanković <vuks89@gmail.com>
2014-09-13 12:40:58 +0200

    Bug fixing

--- src/main/java/com/vukstankovic/professionalorientation/RecommenderAPI.java
+++ src/main/java/com/vukstankovic/professionalorientation/RecommenderAPI.java
@@ -21,9 +21,11 @@ public class RecommenderAPI {
     @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public String getSimilar(@QueryParam("id") String id){
 		Recommender rc = new Recommender();
+		Recommendations rcm = new Recommendations();
 		rc.calculateSimilarity(Integer.parseInt(id));
 		ArrayList<User> similarUsers = rc.getSimilarUsers();
-		String json = new Gson().toJson(similarUsers);
+		ArrayList<Choice> choices = rcm.getChoices(similarUsers);
+		String json = new Gson().toJson(choices);
 		return json;
 	}
 }
